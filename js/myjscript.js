83dd6e5d39e9ab100fddaccfb9ddf03cfec3517e
tasingh <tasingh@aarp.org>
2017-04-15 16:01:00 -0400

    added scrollbar functionality

--- js/myjscript.js
+++ js/myjscript.js
@@ -2,14 +2,15 @@ var tsidhu = new function(){
     $(window).scroll(function () {
         var scrollTop, headerTopHeight, diff;
 
-        scrollTop = $('window').scrollTop();
-        headerTopHeight = $('#header-nav').offset().top();
+        scrollTop = $(window).scrollTop();
+        headerTopHeight = $('#header-nav').offset().top;
         diff = headerTopHeight - scrollTop;
 
         if(diff <= 0){
-            $('#header-nav').css({"position" : "fixed","top" : "0"});
+            console.log(diff);
+            $('#header-nav .container').css({"position" : "fixed" , "top" : "0"});
         } else {
-            $('#header-nav').css('position' , 'relative');
+            $('#header-nav .container').css('position' , 'relative');
         }
     });
 }
